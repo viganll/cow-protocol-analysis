e59ce156bf07dd1bb833a360b1aa8412c1a01c58
Vigan Lladrovci <v.lladrovci@finway.de>
2023-07-16 17:28:50 +0200

    ✨ Adds error and warn file logger

--- winstonLogger.ts
+++ winstonLogger.ts
@@ -2,6 +2,7 @@ import { createLogger, format, transports } from "winston";
 
 const winstonLogger = createLogger({
   level: "info",
+  transports: [new transports.File({ filename: "log/error.log", level: "error" }), new transports.File({ filename: "log/warn.log", level: "warn" })],
 });
 
 let consoleTransport: transports.ConsoleTransportInstance;
@@ -14,6 +15,7 @@ consoleTransport = new transports.Console({
   ),
   level: "debug",
 });
+
 winstonLogger.add(consoleTransport);
 
 export const logger = winstonLogger;

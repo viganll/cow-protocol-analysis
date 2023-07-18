eb9709646ab9fa501b209864b21552caa63ff6ee
Vigan Lladrovci <v.lladrovci@finway.de>
2023-07-18 13:38:29 +0200

    ♻️ Refactor Liquidity Calculator

--- CommandLineInterface.ts
+++ CommandLineInterface.ts
@@ -2,6 +2,8 @@ import { Command } from "commander";
 import { cowProtocolService } from "./CoWProtocolService";
 import { flashbotsBlockService } from "./FlashbotsService";
 import { liquidityCalculator } from "./LiquidityCalculator";
+import { priceService } from "./PriceService";
+import { getCoWProtocolSettelementsWithTx, partitionRange, writeJSONFile } from "./utils";
 const program = new Command();
 
 class CommandLineInterface {
@@ -10,21 +12,43 @@ class CommandLineInterface {
       .command("calculate-cow-tx <tx>")
       .description("It calculates internal liquidity of tx")
       .action(async (tx) => {
-        await liquidityCalculator.getInternalLiquidityOfCoWProtocolTransaction(tx);
+        const cowBatch = await liquidityCalculator.getInternalLiquidityOfCoWProtocolTransaction(tx);
+        console.log(cowBatch);
       });
 
     program
       .command("calculate-cow <startBlock> <endBlock>")
       .description("It calculates internal liquidity of CoW Protocol from startBlock to endBlock")
       .action(async (startBlock, endBlock) => {
-        await liquidityCalculator.getInternalLiquidityOfCoWProtocol(parseInt(startBlock), parseInt(endBlock));
+        const partitions = partitionRange(parseInt(startBlock), parseInt(endBlock), 10000);
+
+        for await (const { start, end } of partitions) {
+          const cowBatches = await liquidityCalculator.getInternalLiquidityOfCoWProtocol(start, end);
+          writeJSONFile(`cow_batches/${start}_${end}.json`, cowBatches);
+        }
+      });
+
+    program
+      .command("calculate-no-interaction <startBlock> <endBlock>")
+      .description("It calculates no interaction of CoW Protocol from startBlock to endBlock")
+      .action(async (startBlock, endBlock) => {
+        await liquidityCalculator.getBatchesSettledWithNoInteractions(parseInt(startBlock), parseInt(endBlock));
       });
 
     program
       .command("calculate-cow-uniswap <startBlock> <endBlock>")
       .description("It calculates internal liquidity of Uniswap V2 from startBlock to endBlock")
       .action(async (startBlock, endBlock) => {
-        await liquidityCalculator.getPotentialInternalLiquidtyOfUniswap(parseInt(startBlock), parseInt(endBlock));
+        const partitions = partitionRange(parseInt(startBlock), parseInt(endBlock), 1000);
+
+        for await (const { start, end } of partitions) {
+          try {
+            const cowBlocks = await liquidityCalculator.getPotentialInternalLiquidtyOfUniswap(start, end);
+            writeJSONFile(`cow_blocks/${start}_${end}.json`, cowBlocks);
+          } catch (err) {
+            // Silent error
+          }
+        }
       });
 
     program
@@ -38,7 +62,28 @@ class CommandLineInterface {
       .command("batches-via-flashbots <startBlock> <endBlock>")
       .description("It finds the batches that were submitted via Flashbots")
       .action(async (startBlock, endBlock) => {
-        await flashbotsBlockService.getBatchesSubmittedViaFlashbots(parseInt(startBlock), parseInt(endBlock));
+        const partitions = partitionRange(parseInt(startBlock), parseInt(endBlock), 100);
+
+        for await (const { start, end } of partitions) {
+          const batchesInBundles = await flashbotsBlockService.getBatchesSubmittedViaFlashbots(start, end);
+          if (batchesInBundles?.length > 0) {
+            writeJSONFile(`batches_in_bundles/${start}_${end}.json`, batchesInBundles);
+          }
+        }
+      });
+
+    program
+      .command("get-cow-settelements <startBlock> <endBlock>")
+      .description("It finds cow settelements")
+      .action(async (startBlock, endBlock) => {
+        await getCoWProtocolSettelementsWithTx(parseInt(startBlock), parseInt(endBlock));
+      });
+
+    program
+      .command("get-order-prices <token>")
+      .description("It gets the orders prices in USD")
+      .action(async (token) => {
+        await priceService.getUSDPrice(token);
       });
 
     program.parse();
--- LiquidityCalculator.ts
+++ LiquidityCalculator.ts
@@ -1,31 +1,52 @@
 import BigNumber from "bignumber.js";
 import { BigNumber as BigNumberE, providers } from "ethers";
 import { provider } from ".";
-import { COW_PROTOCOL_TRADE_EVENT, UNISWAP_V2_SWAP_EVENT, UNISWAP_V3_SWAP_EVENT } from "./const";
+import { COW_PROTOCOL_INTERACTION_EVENT, COW_PROTOCOL_TRADE_EVENT, UNISWAP_V2_SWAP_EVENT } from "./const";
 import { cowProtocol } from "./dexs/cowProtocol";
 import { UniswapV2 } from "./dexs/uniswapV2";
 import { UniswapV3 } from "./dexs/uniswapV3";
-import { getCoWProtocolSettelements, getDex } from "./utils";
+import { CoWBatch, CoWBlock, TokenOut } from "./types";
+import { buildCoWBatch, buildCoWBlock, buildTokenOut, getCoWProtocolSettelements, getDex, getUniswapLogs, groupByProperty, minMax } from "./utils";
 import { logger } from "./winstonLogger";
 
 class LiquidityCalculator {
   async getInternalLiquidityOfCoWProtocol(startBlock: number, endBlock: number) {
     const cowTransactions = await getCoWProtocolSettelements(startBlock, endBlock);
+    let cowBatches: CoWBatch[] = [];
     for (const cowTx of cowTransactions) {
-      await this.getInternalLiquidityOfCoWProtocolTransaction(cowTx);
+      try {
+        const cowBatch = await this.getInternalLiquidityOfCoWProtocolTransaction(cowTx);
+        cowBatches.push(cowBatch);
+      } catch (err) {
+        //Silent err
+      }
     }
+
+    return cowBatches;
   }
 
-  async getInternalLiquidityOfCoWProtocolTransaction(transaction: string) {
+  async getInternalLiquidityOfCoWProtocolTransaction(transaction: string): Promise<CoWBatch> {
     try {
       logger.info(`[CoW Protocol] Calculating internal liqudity for ${transaction}`);
 
       const transactionReceipt = await provider.getTransactionReceipt(transaction);
-      const { totalAmountInByToken, totalAmountOutByToken } = this.getTradeLiquidityOfCoWProtocol(transactionReceipt);
+      const { totalAmountInByToken, totalAmountOutByToken, numberOfTrades, noInteraction } = this.getTradeLiquidityOfCoWProtocol(transactionReceipt);
+
+      const blockNumber = transactionReceipt.blockNumber;
+      const { timestamp } = await provider.getBlock(blockNumber);
+
+      if (numberOfTrades === 1 && !noInteraction) {
+        logger.info(`[CoW Protocol] Skipping tx ${transaction} as it has only one token`);
+        const [token, amountOut] = totalAmountOutByToken.entries().next().value;
+        const tokenOut = buildTokenOut({ token, cowScore: 0, totalAmount: new BigNumber(amountOut.toString()), maxCowScore: 0 });
+        return buildCoWBatch({ transaction, blockNumber, timestamp, noInteraction: false, tokensOut: [tokenOut] });
+      }
 
       const totalAmountInDeXByToken = await this.getDeXInLiquidity(transactionReceipt);
 
-      totalAmountOutByToken.forEach((amountOut, tokenOut) => {
+      let tokensOut: TokenOut[] = [];
+
+      for await (const [tokenOut, amountOut] of totalAmountOutByToken.entries()) {
         const inAmountTokens = totalAmountInByToken.get(tokenOut) ?? BigNumberE.from(0);
         const dexInAmountTokens = totalAmountInDeXByToken.get(tokenOut) ?? BigNumberE.from(0);
 
@@ -33,67 +54,99 @@ class LiquidityCalculator {
         const _dexAmountIn = new BigNumber(dexInAmountTokens.toString());
         const _amountOut = new BigNumber(amountOut.toString());
 
-        const internalLiquidity = _amountIn.minus(_dexAmountIn).dividedBy(_amountOut);
+        const cowScore = minMax(_amountIn.minus(_dexAmountIn).dividedBy(_amountOut).toNumber(), 0, 1);
+        const maxCowScore = minMax(_amountIn.dividedBy(_amountOut).toNumber(), 0, 1);
 
-        logger.info(`[CoW Protocol] Internal Liquidity is ${internalLiquidity.toFixed(2)} for token ${tokenOut}`);
-      });
+        const tokenBuild = buildTokenOut({ token: tokenOut, cowScore, totalAmount: _amountOut, maxCowScore });
+        tokensOut.push(tokenBuild);
+        logger.info(`[CoW Protocol] Internal Liquidity is ${cowScore.toFixed(2)} for token ${tokenOut}`);
+      }
+
+      return buildCoWBatch({ transaction, timestamp, blockNumber, tokensOut, noInteraction });
     } catch (err) {
       logger.error(`Error while calculating internal liqudity of tx ${transaction}: ${err}`);
+      throw err;
     }
   }
 
   async getPotentialInternalLiquidtyOfUniswap(fromBlock: number, endBlock: number) {
-    for (var blockNumber = fromBlock; blockNumber <= endBlock; blockNumber++) {
-      logger.info(`[Uniswap V2] Calculating internal liqudity for block ${blockNumber}`);
+    let cowBlocks: CoWBlock[] = [];
 
-      try {
+    try {
+      const logs = await getUniswapLogs(fromBlock, endBlock);
+      const logsByBlock = groupByProperty(logs, "blockNumber");
+
+      for await (const [block, logs] of Object.entries(logsByBlock)) {
         const totalAmountInByToken = new Map<string, BigNumberE>();
         const totalAmountOutByToken = new Map<string, BigNumberE>();
-        const block = await provider.getBlock(blockNumber);
+        const blockNumber = Number(block);
+        const { timestamp } = await provider.getBlock(blockNumber);
 
-        const transactions = block.transactions;
+        for await (const log of logs) {
+          const logFunctionSelector = log.topics[0];
+          // if (logFunctionSelector !== UNISWAP_V2_SWAP_EVENT && logFunctionSelector !== UNISWAP_V3_SWAP_EVENT) continue;
+          const uniswapDeX = logFunctionSelector === UNISWAP_V2_SWAP_EVENT ? new UniswapV2() : new UniswapV3();
 
-        for (const transaction of transactions) {
-          const transactionReceipt = await provider.getTransactionReceipt(transaction);
-          for (const log of transactionReceipt.logs) {
-            const logFunctionSelector = log.topics[0];
-            if (logFunctionSelector !== UNISWAP_V2_SWAP_EVENT && logFunctionSelector !== UNISWAP_V3_SWAP_EVENT) continue;
-            const uniswapDeX = logFunctionSelector === UNISWAP_V2_SWAP_EVENT ? new UniswapV2() : new UniswapV3();
+          const { tokenIn, amountIn, tokenOut, amountOut } = await uniswapDeX.getSwapAmounts(log);
 
-            const { tokenIn, amountIn, tokenOut, amountOut } = await uniswapDeX.getSwapAmounts(log);
+          const currentAmountIn = totalAmountInByToken.get(tokenIn);
+          const currentAmountOut = totalAmountOutByToken.get(tokenOut);
 
-            const currentAmountIn = totalAmountInByToken.get(tokenIn);
-            const currentAmountOut = totalAmountOutByToken.get(tokenOut);
-
-            currentAmountIn ? totalAmountInByToken.set(tokenIn, currentAmountIn.add(amountIn)) : totalAmountInByToken.set(tokenIn, amountIn);
-            currentAmountOut ? totalAmountOutByToken.set(tokenOut, currentAmountOut.add(amountOut)) : totalAmountOutByToken.set(tokenOut, amountOut);
-          }
+          currentAmountIn ? totalAmountInByToken.set(tokenIn, currentAmountIn.add(amountIn)) : totalAmountInByToken.set(tokenIn, amountIn);
+          currentAmountOut ? totalAmountOutByToken.set(tokenOut, currentAmountOut.add(amountOut)) : totalAmountOutByToken.set(tokenOut, amountOut);
         }
+
+        let tokensOut: TokenOut[] = [];
 
         totalAmountOutByToken.forEach((amountOut, tokenOut) => {
           const amountIn = totalAmountInByToken.get(tokenOut) ?? BigNumberE.from(0);
 
-          const _inAmountTokens = new BigNumber(amountIn.toString());
+          const _amountIn = new BigNumber(amountIn.toString());
           const _amountOut = new BigNumber(amountOut.toString());
 
-          const _internalLiquidity = _inAmountTokens.dividedBy(_amountOut);
-          logger.info(`[Uniswap V2] Internal Liquidity is ${_internalLiquidity.toFixed(2)} for token ${tokenOut}`);
+          const maxCowScore = minMax(_amountIn.dividedBy(_amountOut).toNumber(), 0, 1);
+
+          const tokenBuild = buildTokenOut({ token: tokenOut, cowScore: maxCowScore, totalAmount: _amountOut, maxCowScore });
+          tokensOut.push(tokenBuild);
+
+          logger.info(`[Uniswap V2] Internal Liquidity is ${maxCowScore.toFixed(2)} for token ${tokenOut}`);
         });
 
-        return { totalAmountInByToken, totalAmountOutByToken };
-      } catch (err) {
-        logger.error(`Error while calculating internal liqudity of block ${blockNumber}: ${err}`);
+        cowBlocks.push(buildCoWBlock({ timestamp, blockNumber, tokensOut }));
       }
+    } catch (err) {
+      logger.error(`Error while calculating internal liqudity: ${err}`);
+      throw err;
+    }
+    return cowBlocks;
+  }
+
+  async getBatchesSettledWithNoInteractions(fromBlock: number, endBlock: number) {
+    const cowTransactions = await getCoWProtocolSettelements(fromBlock, endBlock);
+    for (const cowTx of cowTransactions) {
+      const transactionReceipt = await provider.getTransactionReceipt(cowTx);
+      let includesInteractionEvent = false;
+      for (const log of transactionReceipt.logs) {
+        if (log.topics[0] === COW_PROTOCOL_INTERACTION_EVENT) {
+          includesInteractionEvent = true;
+        }
+      }
+
+      if (!includesInteractionEvent) logger.info(`No interaction event ${cowTx}`);
     }
   }
 
   private getTradeLiquidityOfCoWProtocol(transaction: providers.TransactionReceipt) {
     const totalAmountInByToken = new Map<string, BigNumberE>();
     const totalAmountOutByToken = new Map<string, BigNumberE>();
+    let numberOfTrades = 0;
+    let noInteraction = true;
 
     for (const log of transaction.logs) {
+      if (log.topics[0] === COW_PROTOCOL_INTERACTION_EVENT) noInteraction = false;
       if (log.topics[0] !== COW_PROTOCOL_TRADE_EVENT) continue;
       const { amountIn, tokenIn, amountOut, tokenOut } = cowProtocol.getSwapAmounts(log);
+      numberOfTrades++;
 
       const currentAmountIn = totalAmountInByToken.get(tokenIn);
       const currentAmountOut = totalAmountOutByToken.get(tokenOut);
@@ -102,7 +155,7 @@ class LiquidityCalculator {
       currentAmountOut ? totalAmountOutByToken.set(tokenOut, currentAmountOut.add(amountOut)) : totalAmountOutByToken.set(tokenOut, amountOut);
     }
 
-    return { totalAmountInByToken, totalAmountOutByToken };
+    return { totalAmountInByToken, totalAmountOutByToken, numberOfTrades, noInteraction };
   }
 
   private async getDeXInLiquidity(transaction: providers.TransactionReceipt) {
@@ -111,8 +164,8 @@ class LiquidityCalculator {
     for (const log of transaction.logs) {
       const logFunctionSelector = log.topics[0];
       const dex = await getDex(logFunctionSelector, log.address);
+      if (dex === "NOT_SUPPORTED") continue;
 
-      if (dex === "NOT IMPLEMENTED") continue;
       const { amountIn, tokenIn } = await dex.getAmountsIn(log);
       const currentAmountIn = totalAmountInByToken.get(tokenIn);
 

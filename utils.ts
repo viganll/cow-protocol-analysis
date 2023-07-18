92e052c293881730734eb6983f84f9076d877f5e
Vigan Lladrovci <v.lladrovci@finway.de>
2023-07-18 13:37:42 +0200

    ✨ Adds util functions

--- utils.ts
+++ utils.ts
@@ -1,5 +1,9 @@
+import BigNumber from "bignumber.js";
 import { Contract } from "ethers";
+import fs from "fs";
+import moment from "moment";
 import { provider } from ".";
+import { priceService } from "./PriceService";
 import { UNISWAP_V2_ABI } from "./abis";
 import {
   BALANCER_SWAP_EVENT,
@@ -25,6 +29,7 @@ import { PancakeV2 } from "./dexs/pancakeV2";
 import { Sushiswap } from "./dexs/sushiswap";
 import { UniswapV2 } from "./dexs/uniswapV2";
 import { UniswapV3 } from "./dexs/uniswapV3";
+import { CoWBatch, CoWBlock, TokenOut } from "./types";
 
 export async function getDex(functionSelector: string, address: string) {
   switch (functionSelector) {
@@ -52,12 +57,12 @@ export async function getDex(functionSelector: string, address: string) {
         return new PancakeV2();
       }
 
-      return "NOT IMPLEMENTED";
+      return "NOT_SUPPORTED";
     case PANCAKE_V2_SWAP_EVENT:
       return new PancakeV2();
     case MAVERICK_SWAP_EVENT:
     default:
-      return "NOT IMPLEMENTED";
+      return "NOT_SUPPORTED";
   }
 }
 
@@ -81,6 +86,17 @@ export async function getCoWProtocolSettelementsWithTx(fromBlock: number, toBloc
   );
 }
 
+export async function getUniswapLogs(fromBlock: number, toBlock: number) {
+  const filter = {
+    topics: [UNISWAP_V3_SWAP_EVENT],
+    fromBlock,
+    toBlock,
+  };
+
+  const logs = await provider.getLogs(filter);
+  return logs;
+}
+
 export async function getCoWProtocolSettelements(fromBlock: number, toBlock: number) {
   const filter = {
     address: COW_PROTOCOL_ADDRESS,
@@ -92,4 +108,104 @@ export async function getCoWProtocolSettelements(fromBlock: number, toBlock: num
   // Get the transaction history using the filter
   const logs = await provider.getLogs(filter);
   return logs.map((log) => log.transactionHash);
+}
+
+export function buildTokenOut({ token, cowScore, totalAmount, maxCowScore }: { token: string; cowScore: number; maxCowScore: number; totalAmount: BigNumber }): TokenOut {
+  const cowValueInUsd = priceService.convertToUSD(token, totalAmount, cowScore);
+  const valueInUsd = priceService.convertToUSD(token, totalAmount, 1);
+
+  const maxCowValueInUsd = cowScore === maxCowScore ? cowValueInUsd : priceService.convertToUSD(token, totalAmount, maxCowScore);
+
+  return {
+    token,
+    cowScore,
+    totalAmount,
+    valueInUsd: valueInUsd.toNumber(),
+    cowValueInUsd: cowValueInUsd.toNumber(),
+    maxCowValueInUsd: maxCowValueInUsd.toNumber(),
+  };
+}
+
+export function buildCoWBlock({ blockNumber, timestamp, tokensOut }: { blockNumber: number; timestamp: number; tokensOut: TokenOut[] }): CoWBlock {
+  const maxCowValueInUsd = tokensOut.reduce((prev, current) => prev + current.maxCowValueInUsd, 0);
+  const batchValueInUsd = tokensOut.reduce((prev, current) => prev + current.valueInUsd, 0);
+  const maxCowScore = batchValueInUsd !== 0 ? maxCowValueInUsd / batchValueInUsd : 0;
+
+  return {
+    maxCowScore,
+    maxCowValueInUsd,
+    blockNumber,
+    batchValueInUsd,
+    timestamp: formatDate(timestamp),
+    tokensOut,
+  };
+}
+
+export function buildCoWBatch({
+  transaction,
+  blockNumber,
+  timestamp,
+  tokensOut,
+  noInteraction,
+}: {
+  transaction: string;
+  blockNumber: number;
+  timestamp: number;
+  tokensOut: TokenOut[];
+  noInteraction: boolean;
+}): CoWBatch {
+  const cowValueInUsd = tokensOut.reduce((prev, current) => prev + current.cowValueInUsd, 0);
+  const maxCowValueInUsd = tokensOut.reduce((prev, current) => prev + current.maxCowValueInUsd, 0);
+  const batchValueInUsd = tokensOut.reduce((prev, current) => prev + current.valueInUsd, 0);
+
+  const cowScore = batchValueInUsd !== 0 ? cowValueInUsd / batchValueInUsd : 0;
+  const maxCowScore = batchValueInUsd !== 0 ? maxCowValueInUsd / batchValueInUsd : 0;
+
+  return {
+    txHash: transaction,
+    cowScore,
+    maxCowScore,
+    blockNumber,
+    cowValueInUsd,
+    batchValueInUsd,
+    noInteraction: noInteraction && cowScore === 0,
+    timestamp: formatDate(timestamp),
+    tokensOut,
+  };
+}
+
+export function partitionRange(start: number, end: number, range: number) {
+  const ranges: { start: number; end: number }[] = [];
+
+  for (let i = start; i <= end; i += range) {
+    const rangeStart = i;
+    const rangeEnd = Math.min(i + range - 1, end); // Ensure the last range doesn't exceed the end
+
+    ranges.push({ start: rangeStart, end: rangeEnd });
+  }
+
+  return ranges;
+}
+
+export function minMax(value: number, min: number, max: number) {
+  return Math.min(Math.max(value, min), max);
+}
+
+export function groupByProperty<T extends Record<PropertyKey, any>, K extends keyof T>(array: T[], property: K): Record<T[K], T[]> {
+  return array.reduce((result, item) => {
+    const key = item[property];
+    if (!result[key]) {
+      result[key] = [];
+    }
+    result[key].push(item);
+    return result;
+  }, {} as Record<T[K], T[]>);
+}
+
+export function formatDate(timestamp: number) {
+  return moment.unix(timestamp).format();
+}
+
+export function writeJSONFile(fileName: string, data: any) {
+  return fs.writeFileSync(fileName, JSON.stringify(data));
 }
